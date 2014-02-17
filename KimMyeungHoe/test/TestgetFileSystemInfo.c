bf56a55efd64195b2aaf920feda40534f40c527a
kim6515516 <kim6515516@kim6515516-VirtualBox.(none)>
2014-02-17 16:21:27 +0900

    test file modified

--- KimMyeungHoe/test/TestgetFileSystemInfo.c
+++ KimMyeungHoe/test/TestgetFileSystemInfo.c
@@ -34,14 +34,3 @@ void test_getFileSystemsInfo(void)
     free(pCurDiskSpaceStatParce);
 }
 
-/*
-typedef struct  
-{
-    char fileSystemName[STR_SIZE];
-    char blockSize[STR_SIZE];
-    char used[STR_SIZE];
-    char avalable[STR_SIZE];
-    char usedPercent[STR_SIZE];
-    char mountOn[STR_SIZE];
-} diskSpaceStatUnit; 
-*/
